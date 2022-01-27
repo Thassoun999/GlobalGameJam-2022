9e785f37b7198d86c9d7d2a81c8e2026c7415a5e
Tae Hassoun <thassoun000@gmail.com>
2022-01-27 14:55:42 -0500

    Resolving compiler issues, re-instated version, including player movement

---
+++ GGJ22 Team Game/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+    private float moveSpeed = 1;
+
+    private RayCastHit2D hit;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Reset moveDelta -- Difference between current position and where I'm going to be
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction, whether you're going left or right
+        if (moveDelta.x < 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    
+        // hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, )
+
+        // Now we are making it move
+        transform.Translate(moveDelta * Time.deltaTime * moveSpeed);
+    }
+}
