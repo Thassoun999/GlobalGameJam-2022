7f4986b9340438e83819d8b1f42ad7fc4d0d6e61
fhisch <fhisch@gmail.com>
2022-01-30 10:14:57 -0500

    added new sfx soundbanks to unity

--- GGJ22 Team Game/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ GGJ22 Team Game/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,16 +13,46 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID ENTRY_THEME = 2404684705U;
+        static const AkUniqueID PLAY_BOX_DRAG_ENV = 2979191818U;
+        static const AkUniqueID PLAY_DEATH_ENEMY = 589308837U;
+        static const AkUniqueID PLAY_DOOR_CLOSE_ENV = 820567787U;
+        static const AkUniqueID PLAY_DOOR_OPEN_ENV = 1270635665U;
         static const AkUniqueID PLAY_FOOTSTEPS_ENEMY = 3124083878U;
         static const AkUniqueID PLAY_FOOTSTEPS_PLAYER = 3044539581U;
+        static const AkUniqueID PLAY_GRAB_BOX_PLAYER = 2028524334U;
+        static const AkUniqueID PLAY_PICKUP_HEALTH_PLAYER = 2982529553U;
+        static const AkUniqueID PLAY_PICKUP_ITEM_PLAYER = 357306552U;
+        static const AkUniqueID PLAY_PORTAL_TO_DARK_PLAYER = 184164629U;
+        static const AkUniqueID PLAY_PORTAL_TO_LIGHT_PLAYER = 1916480607U;
+        static const AkUniqueID PLAY_PROJECTILE_FLY_ENV = 771295557U;
+        static const AkUniqueID PLAY_PROJECTILE_HIT_ENV = 1781223061U;
+        static const AkUniqueID PLAY_SHOUT_L_ENEMY = 4269470325U;
+        static const AkUniqueID PLAY_SHOUT_S_ENEMY = 3082387274U;
+        static const AkUniqueID PLAY_SWITCH_FLIP_ENV = 800496276U;
+        static const AkUniqueID PLAY_SWORD_CHARGE_READY_PLAYER = 3799651964U;
+        static const AkUniqueID PLAY_SWORD_CHARGE_SWING_PLAYER = 620698609U;
         static const AkUniqueID PLAY_SWORD_PLAYER = 423282211U;
+        static const AkUniqueID PLAY_TAKE_DAMAGE_ENEMY = 343234584U;
+        static const AkUniqueID PLAY_TAKE_DAMAGE_PLAYER = 3411544435U;
+        static const AkUniqueID STOP_BOX_DRAG_ENV = 326667744U;
+        static const AkUniqueID ZERO_THEME = 2227011225U;
     } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BOX_DRAG = 4102969033U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID BOX_DRAG_START = 1123465360U;
+    } // namespace TRIGGERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID ENEMYSFX = 406817756U;
+        static const AkUniqueID ENVSFX = 1794843847U;
         static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID PLAYERSFX = 4283257371U;
     } // namespace BANKS
